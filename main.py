95c3cf1848a16880d2054b03799ce0dcb9ba7ef6
Tyler Smith <smitht8087@uwstout.edu>
2024-02-27 14:37:31 -0600

    Just pushing

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ import pandas as pd
 def create_server_connection(host_name, user_name, user_password):
     connection = None
     try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password
-        )
+        connection = mysql.connector.connect(user='root', password='MyDB2024',
+                              host='localhost', database='HPDataBase')
+       
+
         print("MySQL Database connection successful")
     except Error as err:
         print(f"Error: '{err}'")
 
     return connection
 
-connection = create_server_connection("localhost", "root", "password")
+connection = create_server_connection("localhost", "root", "MyDB2024")
 
-amount = input('Enter an amount: ')
-account = input('Enter an account: ')
-description = input('Enter an description: ')
-category = input('Enter an category: ')
-timestamp = input('Enter an timestamp (Optional): ')
-notes = input('Enter any notes (Optional): ')
+#amount = input('Enter an amount: ')
+#account = input('Enter an account: ')
+#description = input('Enter an description: ')
+#category = input('Enter an category: ')
+#timestamp = input('Enter an timestamp (Optional): ')
+#notes = input('Enter any notes (Optional): ')
 
