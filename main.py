d99750be8d4b13c65c62e279e23a27fcafb68bd9
Liam1017 <elijahturany1415@gmail.com>
2024-03-02 22:58:15 -0600

    Created createDatabase, buildTables, and table add functions

--- main.py
+++ main.py
@@ -42,20 +42,52 @@ def execute_query(connection, query):
     except Error as err:
         print(f"Error: '{err}'")
 
-
 #NOT stolen
-def addTransaction():
-    pass
-def addAccount():
-    pass
-def addUser():
-    pass
-def addCategory():
-    pass
+def createDatabase():
+    connection = create_server_connection("localhost", "root", "MyDB2024")
+    query = "CREATE DATABASE ExpenseTracker"
+    execute_query(connection, query)     
 
+def buildTables(connection):
+    createAccountTable = """
+    CREATE TABLE Account (
+        AccountId int(10) NOT NULL PRIMARY KEY ,
+        title VARCHAR(20) NOT NULL,
+        balance int(15) NOT NULL,
+        UserId int(10) NOT NULL
+    );
+    """
+    createTransactionTable = """
+    CREATE TABLE Transaction (
+        transactionNum int(10) NOT NULL PRIMARY KEY ,
+        amount int(10),
+        description VARCHAR(250),
+        accountId int(10),
+        categoryId int(10),
+        timestamp VARCHAR(30),
+        note VARCHAR(100)
+    );
+    """
+    createCategoryTable = """
+    CREATE TABLE Category (
+        categoryId int(10) NOT NULL PRIMARY KEY ,
+        name VARCHAR(25)
+    );
+    """
+    createUserTable = """
+    CREATE TABLE User (
+        userId int(10) NOT NULL PRIMARY KEY ,
+        numberOfAccounts int(8),
+        levelOfAccess int(2)
+    );
+    """
 
-def clearTables():
-    connection = create_db_connection("localhost", "root", "MyDB2024", "expensetracker")
+    execute_query(connection, createAccountTable)
+    execute_query(connection, createTransactionTable)
+    execute_query(connection, createCategoryTable)
+    execute_query(connection, createUserTable)
+
+def clearTables(connection):
     clearTables = """
         TRUNCATE TABLE Account;
         TRUNCATE TABLE Transaction;
@@ -64,8 +96,7 @@ def clearTables():
     """
     execute_query(connection, clearTables)
 
-def deleteTables():
-    connection = create_db_connection("localhost", "root", "MyDB2024", "expensetracker")
+def deleteTables(connection):
     clearTables = """
         DROP TABLE Account;
         DROP TABLE Transaction;
@@ -74,53 +105,22 @@ def deleteTables():
     """
     execute_query(connection, clearTables)
 
-# connection = create_server_connection("localhost", "root", "password")
-# query = "CREATE DATABASE ExpenseTracker"
-# execute_query(connection, query)
+#Need to add typechecking
+def addAccount(connection, accountId, title, balance, userId):
+    query = "INSERT INTO account VALUES(" + str(accountId) + ",  '" + title + "', " + str(balance) + ", " + str(userId) + ")"
+    execute_query(connection, query)
+    
+def addTransaction(connection, transactionId, amount, description, accountId, categoryId, timestamp, note):
+    query = "INSERT INTO transaction VALUES(" + str(transactionId) + ",  " + str(amount) + ", '" + description + "', " + str(accountId) + ", " + str(categoryId) + ", " + str(timestamp) + ", '" + note +"')"
+    execute_query(connection, query)
+    
+def addCategory(connection, categoryId, name):
+    query = "INSERT INTO category VALUES(" + str(categoryId) + ",  '" + name + "')"
+    execute_query(connection, query)
 
-connection = create_db_connection("localhost", "root", "MyDB2024", "expensetracker")
-
-createAccountTable = """
-CREATE TABLE Account (
-    AccountId int(10) NOT NULL PRIMARY KEY ,
-    title VARCHAR(20) NOT NULL,
-    balance int(15) NOT NULL,
-    UserId int(10) NOT NULL
-);
-"""
-
-createTransactionTable = """
-CREATE TABLE Transaction (
-    transactionNum int(10) NOT NULL PRIMARY KEY ,
-    amount int(10),
-    description VARCHAR(250),
-    accountId int(10),
-    categoryId int(10),
-    timestamp VARCHAR(30),
-    note VARCHAR(100)
-);
-"""
-
-createCategoryTable = """
-CREATE TABLE Category (
-    categoryId int(10) NOT NULL PRIMARY KEY ,
-    name VARCHAR(25)
-);
-"""
-
-createUserTable = """
-CREATE TABLE User (
-    userId int(10) NOT NULL PRIMARY KEY ,
-    numberOfAccounts int(8),
-    levelOfAccess int(2)
-);
-"""
-
-#deleteTables()
-#execute_query(connection, createAccountTable)
-#execute_query(connection, createTransactionTable)
-#execute_query(connection, createCategoryTable)
-#execute_query(connection, createUserTable)
+def addUser(connection, userId, numAccounts, levelOfAccess):
+    query = "INSERT INTO user VALUES(" + str(userId) + ",  " + str(numAccounts) + ",  " + str(levelOfAccess) +")"
+    execute_query(connection, query)
 
 # amount = input('Enter an amount: ')
 # account = input('Enter an account: ')
@@ -128,3 +128,11 @@ CREATE TABLE User (
 # category = input('Enter an category: ')
 # timestamp = input('Enter an timestamp (Optional): ')
 # notes = input('Enter any notes (Optional): ')
+
+createDatabase()
+connection = create_db_connection("localhost", "root", "MyDB2024", "expensetracker")
+buildTables(connection)
+addUser(connection, 0, 0, 1)
+addAccount(connection, 0, "Wallet", 9, 0)
+addCategory(connection, 0, "Snacks")
+addTransaction(connection, 0, 2, "Candy bar", 0, 0, 100, "")
